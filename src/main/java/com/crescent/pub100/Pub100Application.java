eb34692bd2496fa7543b39d1d1c2a919aeab7107
s.sharon <sharonpriya@bsauniv.ac.in>
2017-05-18 14:34:22 +0530

    first commit

---
+++ src/main/java/com/crescent/pub100/Pub100Application.java
@@ -0,0 +1,12 @@
+package com.crescent.pub100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pub100Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pub100Application.class, args);
+	}
+}
---
+++ src/test/java/com/crescent/pub100/Pub100ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.crescent.pub100;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Pub100ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
